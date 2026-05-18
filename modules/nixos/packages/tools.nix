265e6e34a476d3daebc16589dd988fbbb84dd7c3
ejhay26 <ejperez623@gmail.com>
2026-05-18 10:42:49 +0800

    Update tools.nix to add missing packages and set Papirus as the default icon theme

--- modules/nixos/packages/tools.nix
+++ modules/nixos/packages/tools.nix
@@ -22,11 +22,13 @@
     libdbusmenu-gtk3
     gparted
     exfatprogs
+    # wireshark - the package hash is currently broken, so it's commented out for now
     ntfs3g
     android-tools
     gptfdisk
     dmg2img
     ngrok
+    docker
     samba
     gnirehtet
     guitarix
@@ -38,15 +40,27 @@
     discord
     telegram-desktop
     qbittorrent
-    protonvpn-gui
+    proton-vpn
     easyeffects
     inkscape
     bibata-cursors
+    papirus-icon-theme
+    # thunar
+    kdePackages.dolphin
+    mission-center
 
   ] ++ [
     # inputs.kwin-effects-forceblur.packages.${pkgs.stdenv.hostPlatform.system}.default
     # inputs.kwin-effects-glass.packages.${pkgs.stdenv.hostPlatform.system}.default
-    inputs.openclaw.packages.${pkgs.stdenv.hostPlatform.system}.default
+    # inputs.openclaw.packages.${pkgs.stdenv.hostPlatform.system}.default
     # inputs.noctalia-shell.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
+
+  # Set Papirus as the default icon theme for GTK and Qt
+  environment.variables = {
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    GTK_ICON_THEME = "Papirus";
+    QT_QPA_PLATFORMTHEME = "gtk2";
+    QT_STYLE_OVERRIDE = "gtk2";
+  };
 }
