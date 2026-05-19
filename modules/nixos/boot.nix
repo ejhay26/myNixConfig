c54a8e56e79bfc605badede5573f3afe56aba15a
ejhay26 <ejperez623@gmail.com>
2026-05-20 02:50:38 +0800

    Refactor boot.nix to improve structure and readability of boot configuration

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,35 +1,49 @@
 { config, lib, pkgs, ... }:
 {
-  boot.initrd.systemd.enable = true;
-  boot.initrd.availableKernelModules = [ "evdev" ];
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 1;
-  boot.loader.timeout = 0;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelParams = [
-    "quiet"
-    "splash"
-    "rd.systemd.show_status=auto"
-    "udev.log_priority=3"
-    "vt.global_cursor_default=0"
-    "nowatchdog"
-  ];
-  boot.extraModprobeConfig = ''
-    options snd_hda_intel model=dell-headset-multi
-  '';
-  boot.supportedFilesystems = [ "ntfs" "exfat" ];
-  boot.kernelModules = [ "usb_storage" "uas" "sd_mod" "sg" ];
+  boot = {
+    initrd = {
+      systemd.enable = true;
+      availableKernelModules = [ "evdev" ];
+    };
 
-  # Enable Plymouth for boot splash screen
-  boot.plymouth = {
-    enable = true;
-    theme = "bgrt"; #bgrt is the default theme
-    themePackages = with pkgs; [
-      # By default we only have the themes in the plymouth package.
-      # You can add other packages here.
-      adi1090x-plymouth-themes
-      # spinfinity-plymouth-themes
+    loader = {
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 1;
+      };
+      timeout = 0;
+      efi.canTouchEfiVariables = true;
+    };
+
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    kernelParams = [
+      "quiet"
+      "splash"
+      "rd.systemd.show_status=auto"
+      "udev.log_priority=3"
+      "vt.global_cursor_default=0"
+      "nowatchdog"
     ];
+
+    extraModprobeConfig = ''
+      options snd_hda_intel model=dell-headset-multi
+    '';
+
+    supportedFilesystems = [ "ntfs" "exfat" ];
+    
+    kernelModules = [ "usb_storage" "uas" "sd_mod" "sg" ];
+
+    # Enable Plymouth for boot splash screen
+    plymouth = {
+      enable = true;
+      theme = "bgrt"; #bgrt is the default theme
+      themePackages = with pkgs; [
+        # By default we only have the themes in the plymouth package.
+        # You can add other packages here.
+        adi1090x-plymouth-themes
+        # spinfinity-plymouth-themes
+      ];
+    };
   };
 }
