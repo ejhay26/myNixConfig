e9f2c2d76a946e75ca348a5d6445195d28bbfae7
ejhay26 <ejperez623@gmail.com>
2026-05-20 02:48:34 +0800

    Comment out Plasma 6 desktop manager in desktop.nix

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -19,7 +19,7 @@
 
   # ========== DESKTOP ENVIRONMENT ==========
 
-  services.desktopManager.plasma6.enable = true;
+  # services.desktopManager.plasma6.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.displayManager.sddm.enable = true;
 
