677d24ecceace8ea25246c2787b43003236ec578
ejhay26 <ejperez623@gmail.com>
2026-07-16 12:07:40 +0800

    Add docker group to terajaki user extraGroups

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -2,7 +2,7 @@
 {
   users.users.terajaki = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "libvirtd" "kvm" "adbusers" "input" "video" "audio" ];
+    extraGroups = [ "wheel" "libvirtd" "kvm" "adbusers" "input" "video" "audio" "docker" ];
     packages = with pkgs; [ tree ];
     shell = pkgs.zsh;
   };
